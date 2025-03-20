f644a7098b21ca2212efa163b0030ac6cc4cef02
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-03-20 13:56:29 +0100

    Add files via upload

--- exercise/PrimeNumberSearcher.java
+++ exercise/PrimeNumberSearcher.java
@@ -3,48 +3,103 @@ package javacore.chapter03.exercise;
 public class PrimeNumberSearcher {
     public static void main(String[] args) {
 
-    // trouver le plus petit diviseur
+                      /** trouver le plus petit diviseur **/
 
-        int dividende = 512;
-        
-        for (int  diviseur = 2;  diviseur < dividende; diviseur++) {
+        int dividende = 12;
 
-            if (dividende %  diviseur == 0) {
+        for (int  diviseurPotentiel = 2; diviseurPotentiel < dividende; diviseurPotentiel++) {
+          if (dividende % diviseurPotentiel == 0) {
 
-                System.out.println("le diviseur est : " + diviseur);
-
-            }
-
+             System.out.println("le plus petit diviseur est : " + diviseurPotentiel);
+              diviseurPotentiel = dividende;
+          }
         }
 
 
 
-      //  exercice bonus  les 50 chiffres premiers
+/** le resultat le plus petit diviseur est : 2  */
+
+        /** trouver les 50 1er nombre 1er **/
 
 
-        // ca fonctionne pas
 
-        int numberIteration   = 50;
-        int countNumberPrime  = 0;
-            dividende         = 2;
+         int number = 2;
+         int countLoop = 0;
+         int numberLoopMax = 50;
 
-        while(countNumberPrime < numberIteration ){
+        while (countLoop< numberLoopMax){
 
-            for (int diviseur = 1; diviseur < dividende ; diviseur++){
+            boolean nomberPrime = true;
+            for (int  diviseurPotentiel = 2; diviseurPotentiel < number; diviseurPotentiel++) {
 
-                if (dividende % diviseur == 0) {
-
-                     System.out.println(diviseur);
-                      dividende++;
-
+                if (number % diviseurPotentiel == 0) {
+                    nomberPrime =false;
                 }
+
             }
 
-            countNumberPrime++;
+            if(nomberPrime){
 
+                System.out.println(number);
+                countLoop++;
+
+            }
+
+            number++;
         }
 
+/** le resultat 2
+ 3
+ 5
+ 7
+ 11
+ 13
+ 17
+ 19
+ 23
+ 29
+ 31
+ 37
+ 41
+ 43
+ 47
+ 53
+ 59
+ 61
+ 67
+ 71
+ 73
+ 79
+ 83
+ 89
+ 97
+ 101
+ 103
+ 107
+ 109
+ 113
+ 127
+ 131
+ 137
+ 139
+ 149
+ 151
+ 157
+ 163
+ 167
+ 173
+ 179
+ 181
+ 191
+ 193
+ 197
+ 199
+ 211
+ 223
+ 227
+ 229  **/
 
     }
 }
+
 
